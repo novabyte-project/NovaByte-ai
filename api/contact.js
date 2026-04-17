62bc23b57ee33b5c162db6a24e0f01bdf6d32e56
novabyte-project <novabyte888@gmail.com>
2026-04-17 15:10:49 +0530

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -14,13 +14,13 @@ export default async function handler(req, res) {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: "novabyte888@gmail.com",
+            pass: "znqblyduziueqdwt",
         },
     });
 
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: "novabyte888@gmail.com",
         to: "novabyte888@gmail.com",
         subject: `New Contact from ${email}`,
         text: `From: ${email}\n\nMessage:\n${message}`,
